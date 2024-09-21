304af51037f9d43cc23986ac8c86502eeb49fe5c
Nikita Ganzha <nk8bnj@gmail.com>
2024-09-21 16:11:20 +0300

    added users using @Input and @Output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
+import { UserComponent } from './components/user/user.component';
+import { DUMMY_USERS } from '../data/dummy-users';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  users = DUMMY_USERS;
+
+  selectUser(id: string) {
+    console.log(id);
+  }
+}
