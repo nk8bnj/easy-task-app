add1a5c8b4deb36a58c06b8d078aa105a1af4bcc
Nikita Ganzha <nk8bnj@gmail.com>
2024-09-25 11:58:44 +0300

    added dummy users tasks

---
+++ src/app/components/task/task.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Task } from '../../../types/task.interface';
+
+@Component({
+  selector: 'app-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css',
+})
+export class TaskComponent {
+  @Input({ required: true }) task!: Task;
+}
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,13 +1,21 @@
 import { Component, Input } from '@angular/core';
 import { User } from '../../../types/user.interface';
+import { TaskComponent } from '../task/task.component';
+import { DUMMY_TASKS } from '../../../data/dummy-tasks';
+import { Task } from '../../../types/task.interface';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css'
+  styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-@Input({ required: true }) user!: User;
+  @Input({ required: true }) user!: User;
+  tasks = DUMMY_TASKS;
+
+  get selectedUserTasks(): Task[] {
+    return this.tasks.filter((task) => task.userId === this.user.id);
+  }
 }
