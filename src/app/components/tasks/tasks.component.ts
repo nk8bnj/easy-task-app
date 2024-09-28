0160f88d7147ba741edbff5a3b2b5374a468c71e
Nikita Ganzha <nk8bnj@gmail.com>
2024-09-28 15:16:51 +0300

    added modal window for adding new task

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -3,11 +3,12 @@ import { TaskComponent } from '../task/task.component';
 import { DUMMY_TASKS } from '../../../data/dummy-tasks';
 import { User } from '../user/user.model';
 import { Task } from '../task/task.model';
+import { NewTaskComponent } from '../new-task/new-task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
@@ -15,11 +16,21 @@ export class TasksComponent {
   @Input({ required: true }) user!: User;
   tasks = DUMMY_TASKS;
 
+  isAddingTask = false;
+
   get selectedUserTasks(): Task[] {
     return this.tasks.filter((task) => task.userId === this.user.id);
   }
 
   completeTask(taskId: string) {
     this.tasks = this.tasks.filter((task) => task.id !== taskId);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask() {
+    this.isAddingTask = false;
   }
 }
